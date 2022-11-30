3e42e1bf43111b083ab3eb8ccc6f1c0218aef45c
NANO <File changed>
2022-11-30 17:00:00 +0600

    1 submission

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,11 @@
 package com.epam.demo;
 
+import com.sun.tools.javac.util.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+       return args.stream().allMatch(StringUtils::isPositiveNumber);
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,6 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        return NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0;
     }
 }
