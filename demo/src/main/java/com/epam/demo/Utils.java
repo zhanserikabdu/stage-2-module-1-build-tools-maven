6dbb976ee8c60fe38a56ca9b5cb507e41d73e42d
NANO <File changed>
2022-12-05 23:18:17 +0600

    2 submission

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -6,6 +6,6 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       return args.stream().allMatch(StringUtils::isPositiveNumber);
+        return args.stream().allMatch(StringUtils::isPositiveNumber);
     }
 }
